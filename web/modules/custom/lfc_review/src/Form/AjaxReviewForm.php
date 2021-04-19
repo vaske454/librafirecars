1dc7432fb97997fdd8c5da72b4e47db838cc4eb5
Vasilije <vasilijetomovic98@gmail.com>
2021-04-19 14:14:31 +0200

    Added AjaxReviewForm.php file.

---
+++ web/modules/custom/lfc_review/src/Form/AjaxReviewForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\lfc_review\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class AjaxReviewForm extends FormBase {
+  public function getFormId() {
+    // TODO: Implement getFormId() method.
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // TODO: Implement buildForm() method.
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // TODO: Implement submitForm() method.
+  }
+
+}
