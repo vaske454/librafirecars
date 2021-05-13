1883b5adaf46469387ea1ce6f39b51336deda0c8
Vasilije <vasilijetomovic98@gmail.com>
2021-05-13 11:37:16 +0200

    Changed AjaxAdminReviewForm.php file because i wanted to display two submit's in my table.

--- web/modules/custom/lfc_review/src/Form/AjaxAdminReviewForm.php
+++ web/modules/custom/lfc_review/src/Form/AjaxAdminReviewForm.php
@@ -13,7 +13,7 @@ class AjaxAdminReviewForm extends FormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-   //TODO get form id
+   return 'admin_review_form';
   }
 
   /**
@@ -23,12 +23,21 @@ class AjaxAdminReviewForm extends FormBase {
 
     $header = [
       'nid' => t('Nid'),
+      'origin' => t('Origin'),
       'title' => t('Title'),
       'rating' => t('Rating'),
       'communication' => t('Communication'),
       'satisfaction' => t('Satisfaction'),
       'correctness' => t('Correctness'),
       'body' => t('Body'),
+      'accept' => t('Accept'),
+      'deny' => t('Deny')
+    ];
+
+    $form['contacts'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#empty' => $this->t("Don't exist!"),
     ];
 
 
@@ -38,25 +47,97 @@ class AjaxAdminReviewForm extends FormBase {
       ->execute();
     $results = Node::loadMultiple($query);
 
-    $data = [];
+   // $data = [];
+
     foreach($results as $node) {
-      $data[] = [
+      $data = [
         'nid' => $node->get('nid')->value,
         'title' => $node->get('title')->value,
+        'origin' => $node->get('field_origin')->value,
         'rating' => $node->get('field_rating')->value,
         'communication' => $node->get('field_communication')->value,
         'satisfaction' => $node->get('field_satisfaction')->value,
         'correctness' => $node->get('field_correctness')->value,
         'body' => $node->get('body')->value,
       ];
-    }
 
-    $form['table'] = [
-      '#type' => 'table',
-      '#header' => $header,
-      '#rows' => $data,
-      '#empty' => $this->t("Don't exist!"),
-    ];
+      $nid = $data['nid'];
+      //$nid = '';
+      $form['contacts'][$nid]['nid'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('nid')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['origin'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('field_origin')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['title'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('title')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['rating'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('field_rating')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['communication'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('field_communication')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['satisfaction'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('field_satisfaction')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['correctness'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('field_correctness')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['body'] = [
+        '#type' => 'textfield',
+        '#default_value' => $node->get('body')->value,
+        '#attributes' => [
+          ' disabled' => 'disabled',
+        ],
+      ];
+
+      $form['contacts'][$nid]['accept'] = [
+        '#type' => 'submit',
+        '#value' => 'Accept',
+        //          '#submit' => array('formActionAccept')
+      ];
+
+      $form['contacts'][$nid]['deny'] = [
+        '#type' => 'submit',
+        '#value' => 'Deny',
+      ];
+    }
 
     return $form;
   }
